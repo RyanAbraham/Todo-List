55558526b77e6409556a212c5683d87a66c19169
YOUR NAME <ryan_ja@live.ca>
2017-04-06 19:10:30 -0400

    Basic create read server finished

--- server.js
+++ server.js
@@ -2,22 +2,36 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const dotenv = require('dotenv');
 
-MongoClient.connect('link-to-mongodb', (err, database) => {
-  // Start server
+dotenv.config();
+
+var db;
+var MONGO_URI = process.env.MONGOLAB_URI;
+
+MongoClient.connect(MONGO_URI, (err, database) => {
+  if(err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('Listening on 3000');
+  });
 });
 
+app.set('view-engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3000, function() {
-  console.log('listening on 3000');
-});
-
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  db.collection('todos').find().toArray((err, result) => {
+    if(err) return console.log(err);
+    res.render('index.ejs', {todos: result})
+  });
 });
 
 app.post('/add-todo', (req, res) => {
-  console.log(req.body);
+  db.collection('todos').save(req.body, (err, result) => {
+    if(err) return console.log(err);
+    console.log('Added todo to database');
+    res.redirect('/');
+  });
 });
 
