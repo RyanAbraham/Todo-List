c5dd7ad867fabebc9cfd1753dac5acead64363ec
YOUR NAME <ryan_ja@live.ca>
2017-04-10 19:47:04 -0400

    Button added for each individual todo

--- public/main.js
+++ public/main.js
@@ -16,23 +16,28 @@ priority.addEventListener('click', () => {
     .catch(err => console.log(err));
 });
 
-// On click, delete the latest critical todo
-complete.addEventListener('click', () => {
-  fetch('todos', {
-    method: 'delete',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-      "category": "CRITICAL"
-    })
-  })
-    .then(res => {
-      if(res.ok) {
-        return res;
-      } else {
-        throw Error(resp.statusText);
-      }
-    })
-    .then(data => {
-      console.log(data);
-    });
-});
+// Add a listener to each button
+// TODO: remove hardcoded 30 to number of todos
+for(var i=0; i<10; i++) {
+  // On click, delete the latest critical todo
+  document.getElementById("complete-todo" + i)
+    .addEventListener('click', () => {
+      fetch('todos', {
+        method: 'delete',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          "category": "CRITICAL"
+        })
+      })
+      .then(res => {
+        if(res.ok) {
+          return res;
+        } else {
+          throw Error(resp.statusText);
+        }
+      })
+      .then(data => {
+        console.log(data);
+      });
+  });
+}
